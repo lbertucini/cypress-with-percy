89e01a483b5d74589b134c4ed9a44aeec8f28914
Lucas Bertucini <lucas_bertucini@hotmail.com>
2022-11-07 01:04:31 -0300

    implementing account services
    changes into code

--- cypress/e2e/ParaBank Account Services.js
+++ cypress/e2e/ParaBank Account Services.js
@@ -42,6 +42,90 @@ describe('Validation Home', () => {
     cy.loginComponents()
   })
 
+  it('It should register component',() => {
+
+
+  cy.get('input[name="customer.firstName"]')
+    .click()
+    .type(firstName)
+  
+  cy.get('input[name="customer.lastName"]')
+    .click()
+    .type(lastName)
+
+  cy.get('input[name="customer.address.street"]')
+    .click()
+    .type(streetAddress)
+  
+  cy.get('input[name="customer.address.city"]')
+    .click()
+    .type(city)
+
+  cy.get('input[name="customer.address.state"]')
+    .click()
+    .type(state)
+
+  cy.get('input[name="customer.address.zipCode"]')
+    .click()
+    .type(zipCode)
+
+  cy.get('input[name="customer.phoneNumber"]')
+    .click()
+    .type(phone)
+
+  cy.get('input[name="customer.ssn"]')
+    .click()
+    .type(ssn)
+  
+  cy.get('input[name="customer.username"]')
+    .click()
+    .type(username)
+  
+  cy.get('input[name="customer.password"]')
+    .click()
+    .type(password)
+  
+  cy.get('#repeatedPassword')
+    .click()
+    .type(repeatPassword)  
+
+    cy.get('input[value="Register"]')
+    .click()
+
+    cy.get('h1[class="title"]')
+    .contains('Welcome')
+
+    
+  })
+
+})  
+
+describe('Validation Home', () => {
+  
+  beforeEach(() => {
+    cy.visit('https://parabank.parasoft.com/parabank/register.htm')
+  })
+
+  it('It should validate the top menu', () => {
+    
+    cy.topMenu()
+  })
+
+  it('It should validate left menu', () => {
+    
+    cy.leftMenu()
+  })
+
+  it('It should validate the shortcuts on the right side',() => {
+    
+    cy.shortcutIcons()
+  })
+
+  it('It should validate login component',() => {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    cy.loginComponents()
+  })
+
   it.only('It should register component',() => {
 
 
